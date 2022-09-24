9d7d6c01c309d2728ed1a3e748c58c8295e1428a
Kelvin <kelvin.seibt@gmail.com>
2022-09-23 21:33:41 -0300

    Versao 1

--- CMSIS_5-5.7.0/CMSIS/NN/Examples/ARM/arm_nn_examples/cifar10/input_generator.py
+++ CMSIS_5-5.7.0/CMSIS/NN/Examples/ARM/arm_nn_examples/cifar10/input_generator.py
@@ -8,31 +8,18 @@ def load_dataset():
 
 trainX, trainY, testX, testY = load_dataset()
 
-counter = [0 for x in range(10)]
+counter = 0
 images = []
 classes = []
 
-for img, classe in zip(trainX, trainY):
-    if counter[classe[0]] < 3:
-        images.append(img.flatten())
-        classes.append(classe[0])
-        counter[classe[0]]+=1
+for img in trainX:
+    counter += 1
+    images.append(img.flatten())
 
-    if sum(counter) == 1000:
+    if counter == 1000:
         break
 
-for img, classe in zip(trainX, trainY):
-    if counter[classe[0]] < 100:
-        images.append(img.flatten())
-        classes.append(classe[0])
-        counter[classe[0]]+=1
-
-    if sum(counter) == 1000:
-        break
-
-aux = '#define CLASSE {'+str(classes)[1:-1]+'}\n\n'
-
-aux += '#define IMG_DATA {'
+aux = '#define IMG_DATA {'
 for img in images:
     aux += '{'+str(list(img))[1:-1]+'},'
 
